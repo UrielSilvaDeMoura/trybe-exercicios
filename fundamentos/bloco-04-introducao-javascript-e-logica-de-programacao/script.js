8e15523f6d367780f079e8885833aed430f2fab5
UrielSilvaDeMoura <urielsilvademoura@outlook.com>
2022-06-21 18:24:26 -0300

    exercicios de fixação variaveis

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/script.js
@@ -0,0 +1,8 @@
+const myName = 'Uriel';
+const birthCity = 'Florianópolis'
+let birthYear = 1994;
+console.log(myName, birthCity, birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = 'Manaus';
+console.log(birthCity);
