e79f4f4a0e123b9d0910e9739e2c6014a99163c9
UrielSilvaDeMoura <urielsilvademoura@outlook.com>
2022-06-21 19:39:12 -0300

    exercicios de fixação and

--- fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/script.js
+++
@@ -1,8 +0,0 @@
-const myName = 'Uriel';
-const birthCity = 'Florianópolis'
-let birthYear = 1994;
-console.log(myName, birthCity, birthYear);
-birthYear = 2030;
-console.log(birthYear);
-birthCity = 'Manaus';
-console.log(birthCity);
