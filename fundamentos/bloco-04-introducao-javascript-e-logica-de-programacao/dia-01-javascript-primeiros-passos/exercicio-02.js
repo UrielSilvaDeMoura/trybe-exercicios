11e302ecc4fb3b8527601033589715e633acb5d7
UrielSilvaDeMoura <urielsilvademoura@outlook.com>
2022-06-22 18:38:14 -0300

    resolução exercicio 2

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-02.js
@@ -0,0 +1,9 @@
+const primeiroNumero = 20;
+const segundoNumero = 13;
+
+if (primeiroNumero > segundoNumero) {
+    console.log('Este é o maior = ' + primeiroNumero);
+}
+else {
+    console.log("Este é o maior = " + segundoNumero);
+}
