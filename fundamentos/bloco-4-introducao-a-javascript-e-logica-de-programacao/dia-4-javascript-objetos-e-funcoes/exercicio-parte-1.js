de6e00107dab1153e877c633df62f04a1fe95f63
UrielSilvaDeMoura <urielsilvademoura@outlook.com>
2022-06-24 21:46:03 -0300

    exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-parte-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-parte-1.js
@@ -3,7 +3,6 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
-
  //console.log('Bem-vinda, ' + info.personagem);
 
   info.recorrente ='sim';
@@ -13,6 +12,25 @@ let info = {
 //for (let chave in info){
  //   console.log(chave);}
 
-for (let chave in info){
-    console.log(info[chave]);
+// for (let chave in info){
+//     console.log(info[chave]);
+// }
+let info2 = {
+    personagem: 'Tio patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'sim'
+};
+
+for (let concat in info) {
+    if (
+        concat === 'recorrente' &&
+        info.recorrente === 'sim' &&
+        info2.recorrente === 'sim'
+    ){
+        console.log("Ambos recorrentes");
+    }
+    else {
+        console.log(info[concat] + ' e ' + info2[concat]);
+    }
 }
