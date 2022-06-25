8fd70efdb17dd37ead12764bd86f710bc06cde55
UrielSilvaDeMoura <urielsilvademoura@outlook.com>
2022-06-24 21:21:23 -0300

    exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-parte-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-parte-1.js
@@ -10,7 +10,9 @@ let info = {
 
   //console.log(info);
 
-for (let chave in info){
-    console.log(chave);
-}
+//for (let chave in info){
+ //   console.log(chave);}
 
+for (let chave in info){
+    console.log(info[chave]);
+}
